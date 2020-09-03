e7405ebf83c899c6cde52e4f072d82c2e55a43d1
Linus Mjornstedt <carl.mjornstedt@gmail.com>
2020-09-03 16:44:25 +0100

    step 11 done

--- lib/Docking_Station.rb
+++ lib/Docking_Station.rb
@@ -1,12 +1,20 @@
 class DockingStation
 
     def release_bike
+      Bike.new
     end
+
+    def dock(bike)
+      @bike = bike
+    end
+
+    attr_reader :bike
 
 end
 
+
 class Bike
   def working?
+    return true
   end
-
 end
