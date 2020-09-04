8a6ff613b6d12ab585569233f1cdbfe181adf42e
Victoria Jusko <vikjusko@gmail.com>
2020-09-04 15:36:52 +0100

    Chapter 12 solved

--- lib/Docking_Station.rb
+++ lib/Docking_Station.rb
@@ -2,16 +2,17 @@ class DockingStation
   attr_reader :bike
   
   def initialize
-    @bike = [] 
+    @docked_bikes = [] 
   end
   
   def release_bike
-      raise 'docking station empty' if @bike.empty?
-      @bike
+      raise 'docking station empty' if @docked_bikes.empty?
+      @docked_bikes.pop
     end
 
     def dock(bike)
-      @bike = bike
+      @docked_bikes << bike
+      return "bike is docked!"
     end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,25 +4,26 @@ describe DockingStation do
     it { is_expected.to respond_to :release_bike }
 
   it 'releases working bikes' do
-     bike = subject.release_bike
-     expect(bike).to respond_to :working?
+     bike = Bike.new
+     expect(bike.working?).to eq true 
    end
 
    it 'raise an error if docking station empty' do 
-  expect{ subject.release_bike }.to raise_error(RuntimeError)
+  expect { subject.release_bike }.to raise_error
   end
 
 
    it 'docks bikes' do
      bike = Bike.new
-     expect(subject.dock(bike)).to eq bike
+     expect(subject.dock(bike)).to eq ("bike is docked!")
    end
-
-   it 'returns docked bikes' do
-  bike = Bike.new
-  subject.dock(bike)
-  expect(subject.bike).to eq bike
-end
 
   it { is_expected.to respond_to (:bike) }
   end
+
+
+   # it 'returns docked bikes' do
+ # bike = Bike.new
+ # subject.dock(bike)
+  #expect(subject.bike).to eq bike
+#end
